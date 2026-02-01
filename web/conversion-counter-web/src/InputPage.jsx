5dbb2fe995e25ac372d87e94c79be7ca76390cc2
Tyler Pierce <ecreipt@gmail.com>
2026-01-31 21:33:12 -0700

    Added basic layout to stats page

--- web/conversion-counter-web/src/InputPage.jsx
+++ web/conversion-counter-web/src/InputPage.jsx
@@ -1,10 +1,18 @@
 import DatePicker from "react-datepicker"
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import './App.css';
 
 function InputPage() {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [inputType, setInputType] = useState('')
+  const [inputType, setInputType] = useState('');
+  const [conversionType, setConversionType] = useState('Accessory');
+  const [itemName, setItemName] = useState('');
+  const [numAppointments, setNumAppointments] = useState();
+
+  function handleSubmit() {
+    // Do AWS database addition
+  }
 
   return (
     <>
@@ -19,8 +27,50 @@ function InputPage() {
         <option value={'Appointments'}>Number of Appointments</option>
       </select>
 
-      {inputType == 'Conversion' && <p>Conversion Selected</p>}
-      {inputType == 'Appointments' && <p>Appointments Selected</p>}
+      {inputType == 'Conversion' &&
+        <>
+          <div>
+            <button onClick={() => setConversionType('Accessory')}>
+              Accessory
+            </button>
+            <button onClick={() => setConversionType('AppleCare')}>
+              AppleCare
+            </button>
+            <button onClick={() => setConversionType('Trade-In')}>
+              Trade-In
+            </button>
+            <button onClick={() => setConversionType('Upgrade')}>
+              Upgrade
+            </button>
+          </div>
+
+          {conversionType == 'Accessory' &&
+            <input placeholder="Enter accessory that was purchased" value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'AppleCare' &&
+            <input placeholder="Enter device AppleCare was added to" value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'Trade-In' &&
+            <input placeholder="Enter device that was traded-in" value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'Upgrade' &&
+            <input placeholder="Enter device that was purchased" value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+
+          <button onClick={handleSubmit}>
+            Add item
+          </button>
+        </>
+      }
+
+      {inputType == 'Appointments' &&
+        <div>
+          <input placeholder="Enter number of appointments" value={numAppointments} onChange={(event) => setNumAppointments(event.target.value)} />
+          <button onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      }
     </>
   )
 }
--- web/conversion-counter-web/src/StatsPage.jsx
+++ web/conversion-counter-web/src/StatsPage.jsx
@@ -1,8 +1,36 @@
+import { HStack, VStack } from "@chakra-ui/react";
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
+
 function StatsPage() {
+  const [searchDate, setSearchDate] = useState(/* Today's date */)
+
   return (
-    <h1>
-      Stats Page
-    </h1>
+    <VStack>
+      <h1>
+        Stats Page
+      </h1>
+      <HStack>
+        <button>
+          Filter
+        </button>
+        <button>
+          Edit
+        </button>
+      </HStack>
+      <h2>
+        Date
+      </h2>
+      <ScrollView>
+        {/* List of items for date */}
+      </ScrollView>
+      <p>Appointments: </p>
+      <p>Conversion %: </p>
+      <HStack>
+        <DatePicker selected={searchDate} onChange={(date) => setSearchDate(date)} />
+        {/* Stepper for date */}
+      </HStack>
+    </VStack>
   )
 }
 
