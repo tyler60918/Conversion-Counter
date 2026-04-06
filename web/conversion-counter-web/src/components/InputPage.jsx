9f90be4ce56d86afba9e50ca5a72915ae2bdc826
Tyler Pierce <ecreipt@gmail.com>
2026-04-06 10:03:07 -0600

    Needs more styling, but updated input page

--- web/conversion-counter-web/src/components/InputPage.jsx
+++ web/conversion-counter-web/src/components/InputPage.jsx
@@ -1,20 +1,21 @@
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useState } from "react";
-import '../stylesheets/App.css';
+import '../stylesheets/InputPage.css';
 import { useAuth } from "./ui/AuthContext";
 import { supabase } from "./ui/supabase";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from "dayjs";
-import { VStack, HStack, Button } from '@chakra-ui/react';
+import { VStack, HStack, Button, Box } from '@chakra-ui/react';
 
 function InputPage() {
   const [selectedDate, setSelectedDate] = useState(dayjs());
-  const [inputType, setInputType] = useState('');
+  const [selectedButton, setSelectedButton] = useState("")
   const [conversionType, setConversionType] = useState('Accessory');
   const [itemName, setItemName] = useState('');
   const [numAppointments, setNumAppointments] = useState("");
   const { user } = useAuth();
+  const convOptions = ['Accessory', 'AppleCare', 'Trade-In', 'Upgrade']
 
   const handleConversionSubmit = async () => {
     // Do Supabase database addition to conversions
@@ -54,73 +55,80 @@ function InputPage() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <VStack spacing={5}>
-        <h1>
-          Input Page
-        </h1>
-        <DatePicker
-          label="Input Date"
-          value={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          slotProps={{
-            textField: { fullWidth: true }
-          }}
-        />
-        <p>Selected Date: {selectedDate?.format("DD MMMM, YYYY")}</p>
-        <select
-          value={inputType}
-          onChange={(event) => setInputType(event.target.value)}
-        >
-          <option value="" disabled>Select an input type</option>
-          <option value={'Conversion'}>Conversion</option>
-          <option value={'Appointments'}>Number of Appointments</option>
-        </select>
-
-        {inputType == 'Conversion' &&
-          <>
-            <HStack spacing={4}>
-              <Button onClick={() => setConversionType('Accessory')}>
-                Accessory
-              </Button>
-              <Button onClick={() => setConversionType('AppleCare')}>
-                AppleCare
-              </Button>
-              <Button onClick={() => setConversionType('Trade-In')}>
-                Trade-In
-              </Button>
-              <Button onClick={() => setConversionType('Upgrade')}>
-                Upgrade
-              </Button>
-            </HStack>
-
-            {conversionType == 'Accessory' &&
-              <input placeholder=" Enter accessory that was purchased " value={itemName} onChange={(event) => setItemName(event.target.value)} />
-            }
-            {conversionType == 'AppleCare' &&
-              <input placeholder=" Enter device AppleCare was added to " value={itemName} onChange={(event) => setItemName(event.target.value)} />
-            }
-            {conversionType == 'Trade-In' &&
-              <input placeholder=" Enter device that was traded-in " value={itemName} onChange={(event) => setItemName(event.target.value)} />
-            }
-            {conversionType == 'Upgrade' &&
-              <input placeholder=" Enter device that was purchased " value={itemName} onChange={(event) => setItemName(event.target.value)} />
-            }
-
-            <Button onClick={(inputType == 'Conversion') ? handleConversionSubmit : handleApptsSubmit}>
-              Add item
-            </Button>
-          </>
-        }
-
-        {inputType == 'Appointments' &&
+        <h2>
+          Log Conversion
+        </h2>
+        <p>Record a sale from your Genius Bar Appointment</p>
+        <Box className="appointment-box">
+          <DatePicker
+            label="Input Date"
+            value={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+            slotProps={{
+              textField: { fullWidth: true }
+            }}
+          />
+          <label htmlFor='apptField'>Appointments Taken</label>
+          <input type='number' placeholder='0' min={0} id='apptField' value={numAppointments} onChange={(event) => setNumAppointments(event.target.value)} />
+          <Button onClick={handleApptsSubmit} >
+            Save
+          </Button>
+          <p>Current: {numAppointments}</p>
+        </Box>
+        <Box className="conversion-box">
+          <h2>Sale Type</h2>
           <VStack>
-            <input placeholder="Enter number of appointments" value={numAppointments} onChange={(event) => setNumAppointments(event.target.value)} />
-            <Button onClick={(inputType == 'Conversion') ? handleConversionSubmit : handleApptsSubmit}>
-              Submit
-            </Button>
+            <HStack>
+              {convOptions.slice(0, 2).map((option) => (
+                <Button
+                  key={option}
+                  onClick={() => {
+                    setSelectedButton(option)
+                    setConversionType(option)
+                  }}
+                  bg={selectedButton === option ? '#2F6DDE' : '#fafafa'}
+                  color={selectedButton === option ? 'white' : 'black'}
+                >
+                  {option}
+                </Button>
+              ))}
+            </HStack>
+            <HStack>
+              {convOptions.slice(2, 4).map((option) => (
+                <Button
+                  key={option}
+                  onClick={() => {
+                    setSelectedButton(option)
+                    setConversionType(option)
+                  }}
+                  bg={selectedButton === option ? '#2F6DDE' : '#fafafa'}
+                  color={selectedButton === option ? 'white' : 'black'}
+                >
+                  {option}
+                </Button>
+              ))}
+            </HStack>
           </VStack>
-        }
+
+          <h2>Item Description</h2>
+          {conversionType == 'Accessory' &&
+            <input placeholder=" Enter accessory that was purchased " value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'AppleCare' &&
+            <input placeholder=" Enter device AppleCare was added to " value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'Trade-In' &&
+            <input placeholder=" Enter device that was traded-in " value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          {conversionType == 'Upgrade' &&
+            <input placeholder=" Enter device that was purchased " value={itemName} onChange={(event) => setItemName(event.target.value)} />
+          }
+          <Button onClick={handleConversionSubmit}>
+            Add Conversion
+          </Button>
+        </Box>
       </VStack>
-    </LocalizationProvider>
+    </LocalizationProvider >
   )
 }
 
